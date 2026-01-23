501bc3071b578ab4d8db42d24331c1287cdaa023
Shafiul Alam <shafiulalams@gmail.com>
2026-01-23 12:51:49 +0600

    all updates

--- demo-1001-userregistration-and-login/resources/views/auth/login.blade.php
+++
@@ -1,7 +0,0 @@
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="password" name="password" placeholder="Password" required>
-    <button type="submit">Login</button>
-</form>
-
